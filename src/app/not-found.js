17a064454b4e9d1695d0c47b325b9f3f123f361f
nicky-mc <cinimod@gmail.com>
2024-11-15 10:25:18 +0000

    added link on homepage to welcome as a hot fix

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
@@ -15,16 +15,16 @@ export default function NotFoundPage() {
       <main className="flex flex-col items-center gap-4">
         <h1 className="text-4xl font-bold">Page Not Found</h1>
         <p className="text-lg text-center max-w-md">
-          We're sorry, but the page you were looking for doesn't exist. <br />{" "}
-          Maybe next time you'll roll a 20. Sorry!
+          We`&apos;re sorry, but the page you were looking for doesn`&apos;t exist. <br />{" "}
+          Maybe next time you`&apos;ll roll a 20. Sorry!
         </p>
         <div className="flex gap-4 mt-4">
-          <a
+          <Link
             className="rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 transition-colors px-4 py-2"
             href="/"
           >
             Go to Home
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="mt-8">
